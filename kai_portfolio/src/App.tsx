adb9f4d89926237a73b58df57a32213679ac4fc8
Kaifeng Yu <1536771545kai@gmail.com>
2024-11-01 21:58:34 -0400

    set up navigator bar

--- kai_portfolio/src/App.tsx
+++ kai_portfolio/src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      Hello React
-    </div>
+    <>
+    <nav>
+      <ul>
+        <li>
+          <Link to = "/"> Home </Link>
+        </li>
+        <li>
+          <Link to = "/profile"> Profile </Link>
+        </li>
+        <li>
+          <Link to = "/contact"> Contact Me </Link>
+        </li>
+      </ul>
+    </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </>
   );
 }
 
