cb78c99a5a4549e4954a7002f8d0d55b9cfb6d81
Kaifeng Yu <1536771545kai@gmail.com>
2024-11-14 00:08:19 -0500

    update

--- kai_portfolio/src/App.tsx
+++ kai_portfolio/src/App.tsx
@@ -8,25 +8,6 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <>
-      {/* <nav className="bg-blue-500 p-4">
-        <ul className="flex space-x-10">
-          <li>
-            <Link to="/" className="text-white hover:text-gray-300">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile" className="text-white hover:text-gray-300">
-              Profile
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="text-white hover:text-gray-300">
-              Contact Me
-            </Link>
-          </li>
-        </ul>
-      </nav> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
