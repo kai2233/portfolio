48799d024b9307ea0de63e11acaf81b672ae1cf7
kaifeng yu <1536771545kai@gmail.com>
2024-11-14 20:45:00 -0500

    refactor profile section

--- kai_portfolio/src/pages/Home.tsx
+++ kai_portfolio/src/pages/Home.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import Skills from './Skills'
 import NavBar from '../components/NavBar';
-
+import Profile from './Profile';
 const Home = () => {
     return (
         <div className='flex justify-center'>
             <div className='relative w-2/5 h-full bg-slate-50 p-2 opacity-80'>
                 <NavBar/>
-                <section className='flex gap-3 mt-16'>
-                    <img src='DSC02062.jpg' alt='Kai' className='w-auto h-44 mx-6 rounded-lg'/>
-                    <div>
-                    <p className='leading-6 text-lg font-space'>Recent graduated full stack developer with a B.S. degree in Computer Science. Skilled in JavaScript, TypeScript, Python, React, Node.js, Express.js and PostgreSQL</p>
-                    </div>
+                <section>
+                    <Profile/>
                 </section>
                 <section>
                     <Skills/>
--- kai_portfolio/src/pages/Profile.tsx
+++ kai_portfolio/src/pages/Profile.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <div>
-            My Profile
+        <div className='flex gap-3 mt-16'>
+            <img src='DSC02062.jpg' alt='Kai' className='w-auto h-44 mx-6 rounded-lg'/>
+            <div>
+                <p className='leading-6 text-lg font-space mt-5'>Recent graduated full stack developer with a B.S. degree in Computer Science. Skilled in JavaScript, TypeScript, Python, React, Node.js, Express.js and PostgreSQL</p>
+            </div>
         </div>
     );
 };
