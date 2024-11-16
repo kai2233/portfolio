ba80381d273fcd1109643d2c5b8f370678b76a09
kaifeng yu <1536771545kai@gmail.com>
2024-11-16 16:44:22 -0500

    add contact button

--- kai_portfolio/src/components/NavBar.tsx
+++ kai_portfolio/src/components/NavBar.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaLinkedin } from "react-icons/fa6";
 import { SiGithub } from "react-icons/si";
+import { IoMail } from "react-icons/io5";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { email_address } from '../constants';
 
 
 type Props = {
@@ -10,10 +12,14 @@ type Props = {
     handleDarkMode : () => void
 }
 const NavBar: React.FC<Props> = ({isDarkMode, handleDarkMode}) => {
+    const handleEmailClick = () => {
+        const emailAddress = `mailto:${email_address}`;
+        window.location.href = emailAddress;
+      };
     return (
         <div className='flex justify-center'>
             <nav className="fixed">
-                <ul className="relative grid grid-cols-3 items-center justify-center py-2 border border-white/10 backdrop-blur-md rounded-full shadow-2xl">
+                <ul className="relative grid grid-cols-4 items-center justify-center py-2 border border-white/10 backdrop-blur-md rounded-full shadow-2xl">
                     <li className='relative z-10 px-2 text-center py-2 rounded-full cursor-pointer text-neutral-300'>
                         <Link to="https://www.linkedin.com/in/kaifeng-yu02/" target="_blank" className="text-black relative z-20 hover:text-gray-300 active:text-blue-500 flex justify-center items-center gap-x-1 dark:text-slate-100">
                                 <FaLinkedin/>
@@ -27,6 +33,14 @@ const NavBar: React.FC<Props> = ({isDarkMode, handleDarkMode}) => {
                                 <p className='text-sm'>Github</p>
                             </div>
                         </Link>
+                    </li>
+                    <li className='relative z-10 px-2 md:px-4 text-center py-2 rounded-full cursor-pointer text-neutral-300'>
+                        <button onClick={handleEmailClick} className="text-black relative z-20 hover:text-gray-300 active:text-blue-500 flex justify-center items-center gap-x-1 dark:text-slate-100">
+                            <div className='flex justify-center items-center gap-x-1'>
+                                <IoMail/>
+                                <p className='text-sm'>Contact</p>
+                            </div>
+                        </button>
                     </li>
                     <li className='relative z-10 px-2 md:px-4 text-center py-2 rounded-full cursor-pointer text-neutral-300'>
                         <button className='flex justify-center items-center gap-x-1 text-black relative z-20 hover:text-gray-300 active:text-blue-500 dark:text-slate-100' onClick={handleDarkMode}>
