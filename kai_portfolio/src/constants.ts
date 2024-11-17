8120b59d406f596fe72b96dd80ba0449e29131a3
kaifeng yu <1536771545kai@gmail.com>
2024-11-16 22:56:20 -0500

    add projects section

--- kai_portfolio/src/constants.ts
+++ kai_portfolio/src/constants.ts
@@ -1,7 +1,7 @@
 import { IconType } from "react-icons";
 import { IoLogoPython, IoLogoReact, IoLogoNodejs } from "react-icons/io5";
 import { BiLogoJavascript, BiLogoTypescript, BiLogoPostgresql } from "react-icons/bi";
-import { SiExpress, SiRedux, SiTailwindcss, SiGit, SiPostman, SiSwift,  } from "react-icons/si";
+import { SiExpress, SiRedux, SiTailwindcss, SiGit, SiPostman, SiSwift, SiGithub} from "react-icons/si";
 
 
 export const skillSet: Record<string, IconType> = {
@@ -16,6 +16,68 @@ export const skillSet: Record<string, IconType> = {
     'Swift': SiSwift,
     'PostgreSQL': BiLogoPostgresql,
     'Git': SiGit,
+    'Github': SiGithub,
     'Postman': SiPostman
 
 }
+
+export const email_address:string = 'kaifeng.yu02@gmail.com'
+
+type Project = {
+    title: string;
+    image: string;
+    description: string;
+    skills: string[];
+    github: {
+        [key: string]: string | undefined;
+        frontend?: string;
+        backend?: string;
+    };
+    deployment?: string;
+}
+
+export const projects: Project[] = [
+    {
+        title: 'Portfolio',
+        image: 'portfolio.png',
+        description: 'A personal portfolio that presents my projects and skills.',
+        skills: ['TypeScript','React.js', 'TailwindCSS','Shadcn/ui', 'React-Icons'],
+        github: {
+            frontend: 'https://github.com/kai2233/portfolio',
+        },
+        deployment: 'https://kai-portfolio.vercel.app/'
+    },
+    {
+        title: 'FocusEureka',
+        image: 'focusEureka.png',
+        description: 'A mobile app of a social platform where user can discovery interesting spot or amazing events within their city.',
+        skills: ['Swift','SwiftUI', 'UIkit', 'Firebase', 'Node.js', 'Express.js', 'Sequelize.js', 'PostgreSQL'],
+        github: {
+            frontend: 'https://github.com/yuzchen7/FocusEureka_frontend',
+            backend: 'https://github.com/yuzchen7/FocusEureka_backend'
+        }
+    },
+    {
+        title: 'Repo Analytics',
+        image: 'repoAnalytics.png',
+        description: 'A website that measures the performance of project development process on Github.',
+        skills: ['Javascript','React.js', 'Node.js', 'Octokit.js', 'Chart.js', 'Express.js', 'Axios'],
+        github: {
+            frontend: 'https://github.com/karechob/Capstone-II-frontend',
+            backend: 'https://github.com/karechob/Capstone-II-backend'
+        },
+        deployment: 'https://repoanalytics.vercel.app/'
+
+    },
+    {
+        title: 'Ticket Wingman',
+        image: 'ticketWingman.png',
+        description: 'A websites that allows for a more user-friendly flight search experience through travel advice and guidance.',
+        skills: ['Javascript','React.js', 'Node.js', 'Express.js', 'Redux', 'Sequelize.js','PostgreSQL', 'Axios', 'Chart.js'],
+        github: {
+            frontend: 'https://github.com/9Mohamedismail/TicketWingman',
+            backend: 'https://github.com/kai2233/TicketWingMan_backend'
+        },
+        deployment: 'https://ticket-wingman.netlify.app/'
+    },
+]
--- kai_portfolio/src/pages/Projects.tsx
+++ kai_portfolio/src/pages/Projects.tsx
@@ -7,25 +7,69 @@ import {
     CardHeader,
     CardTitle,
 } from '../components/card';
+import { projects } from '../constants';
 
 const Projects = () => {
 
     return (
         <div className='my-16 mx-8'>
             <h1 className='font-semibold text-2xl text-center dark:text-slate-100'>Projects:</h1>
-            <div className='grid grid-cols-2 gap-4 mt-7'>
-                <div className='bg-customWhite p-2 border-2 shadow-lg'>
-                    <h1 className='text-2xl font-bold'>Project 1</h1>
-                    <p className='text-lg'>Description</p>
-                </div>
-                <div className='bg-customWhite p-2 border-2 shadow-lg'>
-                    <h1 className='text-2xl font-bold'>Project 2</h1>
-                    <p className='text-lg'>Description</p>
-                </div>
-                <div className='bg-customWhite p-2'>
-                    <h1 className='text-2xl font-bold'>Project 3</h1>
-                    <p className='text-lg'>Description</p>
-                </div>
+            <div className='columns-2 gap-1'>
+                {projects.map((project) => {
+                    return (
+                        <Card className='break-inside-avoid dark:border-slate-100 mt-1'>
+                            <CardHeader>
+                                <CardTitle>{project.title}</CardTitle>
+                                <CardDescription>
+                                    {project.deployment ? (
+                                        <a
+                                            href={project.deployment}
+                                            target='_blank'
+                                            rel='noreferrer'
+                                            className='block overflow-hidden group'
+                                        >
+                                            <img
+                                                src={project.image}
+                                                alt={project.title}
+                                                className='w-full aspect-square object-cover transition-transform duration-300 ease-in-out group-hover:scale-105'
+                                            />
+                                        </a>
+                                    ) : (
+                                        <img
+                                            src={project.image}
+                                            alt={project.title}
+                                            className='w-full aspect-square object-cover transition-transform duration-300 ease-in-out hover:scale-105'
+                                        />
+                                    )}
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent>{project.description}</CardContent>
+                            <CardContent className='flex flex-wrap gap-2 mt-0'>{project.skills.map((skill) => {
+                                return (
+                                    <span className='bg-gray-200 dark:bg-slate-700 dark:text-white px-2 py-1 rounded-full text-sm'>{skill}</span>
+                                );
+                            })}</CardContent>
+                            <CardFooter className='gap-2'>
+                                {/* {Object.keys(project.github).map((link) => {
+                                    return (
+                                        <a href={project.github[link]} target='_blank' rel='noreferrer' className='border-2 rounded-lg'>{link}</a>
+                                    );
+                                })
+                                } */}
+                                {/* {project.deployment &&
+                                    <a
+                                        href={project.deployment}
+                                        target='_blank'
+                                        rel='noreferrer'
+                                        className='w-full border-2 rounded-lg'
+                                    >
+                                        Visit
+                                    </a>
+                                } */}
+                            </CardFooter>
+                        </Card>
+                    );
+                })}
             </div>
         </div>
     );
