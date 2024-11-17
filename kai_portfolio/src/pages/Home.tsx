e92342922dcb3c9d6c8c98ccc928880117883a20
kaifeng yu <1536771545kai@gmail.com>
2024-11-16 22:43:02 -0500

    add footer

--- kai_portfolio/src/pages/Home.tsx
+++ kai_portfolio/src/pages/Home.tsx
@@ -13,8 +13,8 @@ const Home = () => {
     }
 
     return (
-        <div className={`flex justify-center ${isDarkMode && 'dark'}`}>
-            <div className='absolute w-2/5 h-auto font-space bg-customWhite p-2 dark:bg-gray-800'>
+        <div className={`flex justify-center dark:bg-gray-900 ${isDarkMode && 'dark'}`}>
+            <div className='w-2/5 h-auto font-space bg-customWhite p-2 dark:bg-gray-800'>
                     <NavBar isDarkMode ={isDarkMode} handleDarkMode = {handleDarkMode}/>
                 <section>
                     <Profile/>
@@ -28,6 +28,19 @@ const Home = () => {
                 <section>
                     <Projects/>
                 </section>
+                <section>
+                    <div className='w-full border-2'/>
+                    <div className='mx-8 my-3'>
+                        <h1 className='font-semibold text-xl dark:text-slate-100'>Contact:</h1>
+                        <div className='flex flex-col gap-2 mt-1 dark:text-slate-100'>
+                            <p>Email: kaifeng.yu02@gmail.com</p>
+                            <p>Phone: 347-205-7129</p>
+                        </div>
+                    </div>
+                </section>
+                <footer className='flex justify-center items-center dark:text-slate-100'>
+                    <p>© 2024 Kaifeng Yu.</p>
+                </footer>
             </div>
         </div>
     );
