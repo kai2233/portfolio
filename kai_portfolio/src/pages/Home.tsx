a5b32e8beb13ee465920c1013345344e9ef35b8c
kaifeng yu <1536771545kai@gmail.com>
2024-11-18 21:49:00 -0500

    update to match mobile

--- kai_portfolio/src/pages/Home.tsx
+++ kai_portfolio/src/pages/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
 
     return (
         <div className={`flex justify-center dark:bg-gray-900 ${isDarkMode && 'dark'}`}>
-            <div className='w-2/5 h-auto font-space bg-customWhite p-2 dark:bg-gray-800 max-lg:w-4/6'>
+            <div className='w-2/5 h-auto font-space bg-customWhite p-2 dark:bg-gray-800 max-lg:w-4/6 max-sm:w-full'>
                     <NavBar isDarkMode ={isDarkMode} handleDarkMode = {handleDarkMode}/>
                 <section>
                     <Profile/>
